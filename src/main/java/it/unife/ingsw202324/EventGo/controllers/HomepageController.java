a6970bca9ada47e027a2af47f7e732c011574fb4
saracavicchi <sara02.cavicchi@edu.unife.it>
2024-07-29 19:29:12 +0200

    Correzione homepage

--- src/main/java/it/unife/ingsw202324/EventGo/controllers/HomepageController.java
+++ src/main/java/it/unife/ingsw202324/EventGo/controllers/HomepageController.java
@@ -82,9 +82,4 @@ public class HomepageController {
         return TemplateRestConsumer.callREST("getNextEvents", null, true);
     }
 
-    @GetMapping("/marzel")
-    public String fetchMarzel() {
-        return TemplateRestConsumer.callREST("getElMarzel", null, true);
-
-    }
 }
